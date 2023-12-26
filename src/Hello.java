71b71e57d89cc81416391a56ebb8b6a56829e613
unknown <karapetyanstepa@gmail.com>
2023-12-26 18:02:44 +0300

    initial commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
-public class Main {
+import java.awt.desktop.SystemEventListener;
+
+public class Hello {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
