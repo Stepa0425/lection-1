4a35f6b917f7342a2faa08cf89b0ab5811bfed34
Admin <karapetyanstepa@gmail.com>
2023-12-26 18:48:41 +0300

    good day

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,6 @@ import java.awt.desktop.SystemEventListener;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Добрый день!");
     }
 }
